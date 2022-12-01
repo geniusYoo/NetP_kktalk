04ec50e7d746b318a5fb1c3766b8d72b43085705
geniuus <cross_man@naver.com>
2022-12-01 22:19:44 +0900

    server에서 방만들기 -> client에서 받기 완료

---
+++ NetP09-java2 (1)/JavaObjServer/src/ChatRoom.java
@@ -0,0 +1,12 @@
+
+public class ChatRoom {
+	public String room_id;
+	public String room_userList;
+	public String room_ChatMsgList;
+	
+	
+	public ChatRoom(String room_id, String room_userList) {
+		this.room_id = room_id;
+		this.room_userList = room_userList;
+	}
+}
